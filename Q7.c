85636a9ea11ab4d7039607bd328c88d439fe23aa
Cahal Agarwalla <144279003+CaHHaL@users.noreply.github.com>
2024-03-24 23:57:11 +0530

    Pascal triangles

---
+++ Q7.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+void printPascal(int n)
+{
+    for (int line = 1; line <= n; line++)
+    {
+        for (int space = 1; space <= n - line; space++)
+            printf(" ");
+        int coef = 1;
+        for (int i = 1; i <= line; i++)
+        {
+            printf("%4d", coef);
+            coef = coef * (line - i) / i;
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int n = 5;
+    printPascal(n);
+    return 0;
+}
